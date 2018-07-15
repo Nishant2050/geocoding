9403d6b1e4dfe6cc2040f534a818e1f780ba5ff5
Nishant2050 <38308057+Nishant2050@users.noreply.github.com>
2018-07-16 02:26:13 +0530

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'docfile',
         'USER': 'postgres',
-        'PASSWORD': 'genius333',
+        'PASSWORD': '*****',
         'HOST': '',
         'PORT':'',
     }
